29cb938797ff2278fe1d70044b00a93538cbb167
Haze59240 <mickael.croquet@laposte.net>
2018-05-30 13:54:24 +0200

    Ajout des éléments concernant le panier

---
+++ Basket/BasketTest/BasketOperation/BasketOperation_CalculateBasketAmoutShould.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Basket;
+using Newtonsoft.Json;
+
+static internal class BasketOperation_CalculateBasketAmoutShould
+{
+    public static int CalculateBasketAmout(IList<BasketLineArticle> basketLineArticles)
+    {
+        var amountTotal = 0;
+        foreach (var basketLineArticle in basketLineArticles)
+        {
+// Retrive article from database
+            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            var uri = new UriBuilder(codeBase);
+            var path = Uri.UnescapeDataString(uri.Path);
+            var assemblyDirectory = Path.GetDirectoryName(path);
+            var jsonPath = Path.Combine(assemblyDirectory, "article-database.json");
+            IList<ArticleDatabase> articleDatabases =
+                JsonConvert.DeserializeObject<List<ArticleDatabase>>(File.ReadAllText(jsonPath));
+            var article = articleDatabases.First(articleDatabase =>
+                articleDatabase.Id == basketLineArticle.Id);
+// Calculate amount
+            var amount = 0;
+            switch (article.Category)
+            {
+                case "food":
+                    amount += article.Price * 100 + article.Price * 12;
+                    break;
+                case "electronic":
+                    amount += article.Price * 100 + article.Price * 20 + 4;
+                    break;
+                case "desktop":
+                    amount += article.Price * 100 + article.Price * 20;
+                    break;
+            }
+
+            amountTotal += amount * basketLineArticle.Number;
+        }
+
+        return amountTotal;
+    }
+}
