a0801e3ef08b66ac0fd776c7512fd53ac24b197c
Haze59240 <mickael.croquet@laposte.net>
2018-06-01 20:36:30 +0200

    "Ajout du polymorphisme + Interface de l'étape 7 + création de la factory Create()"

---
+++ Basket/Basket/Domain/ArticleBase.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.Domain
+{
+    public abstract class ArticleBase
+    {
+        protected int _price;
+        protected string _category;
+
+        public ArticleBase(int price, string category)
+        {
+            _price = price;
+            _category = category;
+        }
+
+        public abstract int Calculate();
+    }
+}
---
+++ Basket/Basket/Domain/ArticleFood.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.Domain
+{
+    public class ArticleFood : ArticleBase
+    {
+        public ArticleFood(int price, string category) : base(price, category)
+        {
+        }
+
+        public override int Calculate()
+        {
+            var amount = 0;
+            amount += _price * 100 + _price * 12;
+            return amount;
+        }
+    }
+}
