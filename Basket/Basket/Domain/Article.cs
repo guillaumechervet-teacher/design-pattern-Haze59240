9c6345d3b825abf0a28ffd17fe3520a880869185
Haze59240 <mickael.croquet@laposte.net>
2018-06-01 17:04:39 +0200

    "Ajout de l'étape 5 avec le basketService et les éléments du domaine"

---
+++ Basket/Basket/Domain/Article.cs
@@ -0,0 +1,32 @@
+﻿namespace Basket.Domain
+{
+    public class Article
+    {
+        private readonly int _price;
+        private readonly string _category;
+
+        public Article(int price, string category)
+        {
+            _price = price;
+            _category = category;
+        }
+
+        public int Calculate()
+        {
+            var amount = 0;
+            switch (_category)
+            {
+                case "food":
+                    amount += _price * 100 + _price * 12;
+                    break;
+                case "electronic":
+                    amount += _price * 100 + _price * 20 + 4;
+                    break;
+                case "desktop":
+                    amount += _price * 100 + _price * 20;
+                    break;
+            }
+            return amount;
+        }
+    }
+}
